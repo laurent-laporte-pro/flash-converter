712da57153fc18df78f592352b2844f10a73023b
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-12-06 16:13:59 +0100

    chore: update .gitignore files

--- flash-converter-wf/src/flash_converter_wf/server.py
+++ flash-converter-wf/src/flash_converter_wf/server.py
@@ -7,6 +7,7 @@ Use the following command to start the Celery worker::
 from flash_converter_wf.app import celery_app
 from flash_converter_wf.subtitle.convert_to_subtitles import convert_to_subtitles_task
 from flash_converter_wf.video.convert_to_audio import convert_to_audio_task
+from flash_converter_wf.video.detect_voice import detect_voice_task
 from flash_converter_wf.video.embed_subtitles import embed_subtitles_task
 from flash_converter_wf.video.preflight_check import preflight_check_task
 from flash_converter_wf.video.process_subtitles import process_subtitles_task
@@ -15,6 +16,7 @@ __all__ = [
     "celery_app",
     "preflight_check_task",
     "convert_to_audio_task",
+    "detect_voice_task",
     "process_subtitles_task",
     "embed_subtitles_task",
     "convert_to_subtitles_task",
