a0f75ea2d7b1e2a2de22aa0a18032a3dcf5fe2f6
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-12-30 19:25:19 +0100

    style(ui): reindent source code

--- flash-converter-ui/src/store/video-tasks/videoTasksStore.test.ts
+++ flash-converter-ui/src/store/video-tasks/videoTasksStore.test.ts
@@ -4,16 +4,36 @@
  * Use Vitest to test the videoTasksStore module.
  */
 
-import { saveTasksToStorage, loadTasksFromStorage } from './videoTasksStore'
+import { loadTasksFromStorage, saveTasksToStorage } from './videoTasksStore'
 import { VideoTask } from '../../types/video-tasks/videoTask'
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 describe('videoTasksStore', () => {
-  const task1: VideoTask = { videoName: 'video1.mp4', taskId: '75cb2396-8158-4907-98cb-60348d15d967', taskStatus: 'PENDING' }
-  const task2: VideoTask = { videoName: 'video2.mp4', taskId: 'fa4d622a-d5bc-49a3-a7e6-4021af0f48f2', taskStatus: 'STARTED' }
-  const task3: VideoTask = { videoName: 'video3.mp4', taskId: '8dba3398-6d82-484e-9025-24b92a6fc7d3', taskStatus: 'RETRY' }
-  const task4: VideoTask = { videoName: 'video4.mp4', taskId: '4767b64a-6963-422c-ba90-a111d745f6cd', taskStatus: 'FAILURE' }
-  const task5: VideoTask = { videoName: 'video5.mp4', taskId: '093320c7-6bb1-4c51-8787-f8bbdf3a5e51', taskStatus: 'SUCCESS' }
+  const task1: VideoTask = {
+    videoName: 'video1.mp4',
+    taskId: '75cb2396-8158-4907-98cb-60348d15d967',
+    taskStatus: 'PENDING'
+  }
+  const task2: VideoTask = {
+    videoName: 'video2.mp4',
+    taskId: 'fa4d622a-d5bc-49a3-a7e6-4021af0f48f2',
+    taskStatus: 'STARTED'
+  }
+  const task3: VideoTask = {
+    videoName: 'video3.mp4',
+    taskId: '8dba3398-6d82-484e-9025-24b92a6fc7d3',
+    taskStatus: 'RETRY'
+  }
+  const task4: VideoTask = {
+    videoName: 'video4.mp4',
+    taskId: '4767b64a-6963-422c-ba90-a111d745f6cd',
+    taskStatus: 'FAILURE'
+  }
+  const task5: VideoTask = {
+    videoName: 'video5.mp4',
+    taskId: '093320c7-6bb1-4c51-8787-f8bbdf3a5e51',
+    taskStatus: 'SUCCESS'
+  }
 
   it('should save and load tasks', () => {
     const tasks: VideoTask[] = [task1, task2, task3, task4, task5]
